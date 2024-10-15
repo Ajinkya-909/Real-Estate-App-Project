8c78a528fc30eeb25fe78a7c9a1fee54defccfbc
Ajinkya <ajinkyadeshmukh8686@gmail.com>
2024-10-15 06:12:43 +0530

    Initialisation of git,react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-orange-600">HELLO</h1>
+      <div className="h-3 w-3 bg-slate-800"></div>
+    </>
+  );
+}
+
+export default App;
