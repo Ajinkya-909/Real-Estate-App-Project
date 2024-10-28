11d68b3085ca4c3b8e04d3bb1080b2c45f766c09
Ajinkya <ajinkyadeshmukh8686@gmail.com>
2024-10-28 20:30:53 +0530

    Initiated Header

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import About from "./pages/About";
 import Sign_In from "./pages/Sign_In";
 import Sign_Out from "./pages/Sign_Out";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<Sign_In />} />
-        <Route path="/sign-out" element={<Sign_Out />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<Sign_In />} />
+          <Route path="/sign-out" element={<Sign_Out />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
