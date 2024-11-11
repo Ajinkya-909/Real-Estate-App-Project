f9379ecb5275d6183a4e40723a457ceabb03caff
Ajinkya <ajinkyadeshmukh8686@gmail.com>
2024-11-11 15:32:54 +0530

    completing the user update functionality

--- main/src/pages/Profile.jsx
+++ main/src/pages/Profile.jsx
@@ -1,13 +1,54 @@
 import React from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import "../../src/index.css";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function Profile() {
-  const currentUser = useSelector((state) => state.user.currentUser);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [formdata, setformdata] = useState({});
+  const [UpdateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setformdata({ ...formdata, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}1`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formdata),
+      });
+
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        setUpdateSuccess(false);
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="bg-yellow-200 rounded-3xl mt-6 p-3 max-w-lg mx-auto">
       <h1 className="text-center font-semibold text-3xl my-7">Profile</h1>
-      <form className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
         <img
           className="self-center rounded-full h-24 w-24 object-cover cursor-pointer"
           src={currentUser.avatar}
@@ -18,21 +59,29 @@ export default function Profile() {
           placeholder="User name"
           className="border rounded-lg p-3"
           id="username"
+          onChange={handleChange}
+          defaultValue={currentUser.username}
         />
         <input
           type="email"
           placeholder="Email"
           className="border rounded-lg p-3"
           id="email"
+          onChange={handleChange}
+          defaultValue={currentUser.email}
         />
         <input
-          type="text"
+          type="password"
           placeholder="Password"
           className="border rounded-lg p-3"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-black p-2 text-Off_White rounded-lg hover:opacity-95 disabled:opacity-85">
-          Update
+        <button
+          disabled={loading}
+          className="bg-black p-2 text-Off_White rounded-lg hover:opacity-95 disabled:opacity-85"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
         <div className="p-3 flex justify-between items-center">
           <span className="text-red-700 cursor-pointer font-medium">
@@ -42,6 +91,10 @@ export default function Profile() {
             Sign Out
           </span>
         </div>
+        <p className="text-red-700 font-semibold">{error ? error : ""}</p>
+        <p className="text-green-700 font-semibold">
+          {UpdateSuccess ? "User Updated Successfully" : ""}
+        </p>
       </form>
     </div>
   );
