b0a45749caf575012b08273d574f4d576b855e2b
Ajinkya <ajinkyadeshmukh8686@gmail.com>
2024-12-29 18:21:08 +0530

    delete and update listing condition update

--- api/controlers/listing.controler.js
+++ api/controlers/listing.controler.js
@@ -15,7 +15,7 @@ export const deleteListing = async (req, res, next) => {
   if (!listing) {
     return next(errorHandler(404, "Listing not found"));
   }
-  if (req.user.id !== listing.userRef) {
+  if (req.params.id !== listing.userRef) {
     return next(errorHandler(401, "You can delete your own listing"));
   }
   try {
@@ -31,7 +31,7 @@ export const updateListing = async (req, res, next) => {
   if (!listing) {
     return next(errorHandler(404, "Listing not found"));
   }
-  if (req.user.id !== listing.userRef) {
+  if (req.params.id !== listing.userRef) {
     return next(errorHandler(401, "You can update your own listing"));
   }
 
